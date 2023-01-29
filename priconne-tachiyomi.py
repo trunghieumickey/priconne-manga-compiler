f54ec84376e7e90cd8bcd6a750fcc2ee877f5066
trunghieumickey <trunghieumickey@outlook.com.vn>
2023-01-29 18:30:24 +0700

    Update priconne-tachiyomi.py

--- priconne-tachiyomi.py
+++ priconne-tachiyomi.py
@@ -1,5 +1,4 @@
 import os
-
 with open("URI-LIST.txt","r") as img:
         image = img.read().split("\n")
         image = image[:-1]
@@ -9,7 +8,7 @@ os.system("cwebp cover.jpg -o priconne/cover.webp")
 os.system("cp details.json priconne/details.json")
 
 for i in range(len(image)):
-    os.mkdir(f'priconne/ch{i+1:03}')
-    os.system("cwebp image/"+image[i].split('/')[-1]+f" -o priconne/ch{i+1:03}/image.webp")
+    os.mkdir(f'priconne/Chapter-{i+1:03}')
+    os.system("cwebp image/"+image[i].split('/')[-1]+f" -o priconne/Chapter-{i+1:03}/image.webp")
 
 os.system("zip -9 -r priconne.zip priconne")
